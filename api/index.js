f77f0f6c9c6676df1a44cd53151738da0da8a1ab
abhinavshukla18 <abhinavshukla18@gmail.com>
2026-05-06 01:16:40 +0530

    Fix serverless file serving

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = (req, res) => {
+  const filePath = path.join(__dirname, '../public/index.html');
+  const html = fs.readFileSync(filePath, 'utf8');
+  res.setHeader('Content-Type', 'text/html');
+  res.send(html);
+};
